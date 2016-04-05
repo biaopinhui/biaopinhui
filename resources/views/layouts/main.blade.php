35965413d85770a4fc184a9abc61a9724e41c80f
biaopinhui <biaopinhui@126.com>
2016-04-05 23:50:57 +0800

    use cdn

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,20 +13,20 @@
         <title>@yield('title') - 标品汇</title>
 
         <!-- Bootstrap Core CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ env('BPH_CDN') }}assets/css/bootstrap.min.css">
         
         <!-- Customizable CSS -->
-        <link rel="stylesheet" href="assets/css/main.css">
-        <link rel="stylesheet" href="assets/css/green.css">
-        <link rel="stylesheet" href="assets/css/owl.carousel.css">
-        <link rel="stylesheet" href="assets/css/owl.transitions.css">
-        <link rel="stylesheet" href="assets/css/animate.min.css">
+        <link rel="stylesheet" href="{{ env('BPH_CDN') }}assets/css/main.css">
+        <link rel="stylesheet" href="{{ env('BPH_CDN') }}assets/css/green.css">
+        <link rel="stylesheet" href="{{ env('BPH_CDN') }}assets/css/owl.carousel.css">
+        <link rel="stylesheet" href="{{ env('BPH_CDN') }}assets/css/owl.transitions.css">
+        <link rel="stylesheet" href="{{ env('BPH_CDN') }}assets/css/animate.min.css">
 
         <!-- Fonts -->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
         
         <!-- Icons/Glyphs -->
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ env('BPH_CDN') }}assets/css/font-awesome.min.css">
         
         <!-- Favicon -->
         <link rel="shortcut icon" href="assets/images/favicon.ico">
@@ -52,22 +52,22 @@
     </div><!-- /.wrapper -->
 
     <!-- JavaScripts placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/jquery-1.10.2.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/jquery-migrate-1.2.1.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/bootstrap.min.js"></script>
     <!-- <script src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script> -->
-    <script src="assets/js/gmap3.min.js"></script>
-    <script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/css_browser_selector.min.js"></script>
-    <script src="assets/js/echo.min.js"></script>
-    <script src="assets/js/jquery.easing-1.3.min.js"></script>
-    <script src="assets/js/bootstrap-slider.min.js"></script>
-    <script src="assets/js/jquery.raty.min.js"></script>
-    <script src="assets/js/jquery.prettyPhoto.min.js"></script>
-    <script src="assets/js/jquery.customSelect.min.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-    <script src="assets/js/scripts.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/gmap3.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/bootstrap-hover-dropdown.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/owl.carousel.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/css_browser_selector.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/echo.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/jquery.easing-1.3.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/bootstrap-slider.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/jquery.raty.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/jquery.prettyPhoto.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/jquery.customSelect.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/wow.min.js"></script>
+    <script src="{{ env('BPH_CDN') }}assets/js/scripts.js"></script>
 
 </body>
 </html>
