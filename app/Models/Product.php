6a179a1e76bf09e6c8d2a681d65a56362d062a90
biaopinhui <biaopinhui@126.com>
2016-05-22 23:49:41 +0800

    fixed pagination issue caused by cache

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Request;
 use Cache;
 use DB;
 
@@ -21,7 +22,9 @@ class Product extends Model
 
     static public function getProducts($conditions = [])
     {
-        $cacheKey = 'getProducts_' . md5(serialize($conditions));
+        $page = Request::get('page', 1);
+        $cacheKey = 'getProducts_' . md5(serialize($conditions)) . '_page' . $page;
+        
         if (!($products = Cache::get($cacheKey))) {
         	$builder = Product::select('*');
 
